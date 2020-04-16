ea8633d6aaa51686f474c2beaf41f198b299a4d6
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 09:47:30 -0400

    i am extremely smart

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,6 @@
 module.exports = async (client) => {
 	const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
-		let fetched;
-	do {
-		fetched = await bootchannel.messages.fetch({limit:100})
-		bootchannel.bulkDelete(fetched.size);
-	}
-	while(fetched.size >= 2);
+	let fetched = await bootchannel.messages.fetch({limit:100})
+	bootchannel.bulkDelete(fetched.size);
  	bootchannel.send("Looks like I am back online!")
 }
