03795709ccf401981dbf36c39c451ae93a0bfc00
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 10:12:49 -0400

    go die

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ module.exports = async (client) => {
 	const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
 	let fetched = await bootchannel.messages.fetch({limit:100})
 	bootchannel.bulkDelete(fetched.size).then(() => {}).catch(err => console.error(err))
- 	bootchannel.send("Looks like I am back online!")
+ 	bootchannel.send("@everyone I have rebooted!")
 }
