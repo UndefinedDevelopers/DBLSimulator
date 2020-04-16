fa841a01b6aa19c8a6419cc269f793d3ef20f0c5
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 09:36:40 -0400

    more testy

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,12 @@
 module.exports = (client) => {
-	const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
 	async () => {
-	  let fetched;
-	  do {
-	    fetched = await bootchannel.fetchMessages({limit: 100});
-	    bootchannel.bulkDelete(fetched);
-	  }
-	  while(fetched.size >= 2);
+		const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
+		let fetched;
+		do {
+			fetched = await bootchannel.fetchMessages({limit: 100});
+			bootchannel.bulkDelete(fetched);
+		}
+		while(fetched.size >= 2);
 	}
  	bootchannel.send("Looks like I am back online!")
 }
