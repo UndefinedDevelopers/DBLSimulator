bd06d7b0823d6ff1842c28bf59871b44abca1c7f
CollierDevs <collier.bell20@gmail.com>
2020-04-16 07:03:58 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,33 +8,37 @@ module.exports = {
     example: 'dbl-help help',
 
     code(client, message, args) {
+        let command = args.slice(0).join(' ');
+        if (!command) {
+            let dbl = client.commands.map(cmd => cmd).filter(cmd => cmd.category === 'dbl');
+            dbl.forEach((z,x) => {
+                dbl[x] = z.name;
+            });
+            if (dbl.length === 0) dbl.push(`No commands yet.`);
 
-        let dbl = client.commands.map(cmd => cmd).filter(cmd => cmd.category === 'dbl');
-        dbl.forEach((z,x) => {
-            dbl[x] = z.name;
-        });
-        if (dbl.length === 0) dbl.push(`No commands yet.`);
+            let luca = client.commands.map(cmd => cmd).filter(cmd => cmd.category === 'luca');
+            luca.forEach((z,x) => {
+                luca[x] = z.name;
+            });
+            if (luca.length === 0) luca.push(`No commands yet.`);
 
-        let luca = client.commands.map(cmd => cmd).filter(cmd => cmd.category === 'luca');
-        luca.forEach((z,x) => {
-            luca[x] = z.name;
-        });
-        if (luca.length === 0) luca.push(`No commands yet.`);
+            let self = client.commands.map(cmd => cmd).filter(cmd => cmd.category === 'self');
+            self.forEach((z,x) => {
+                self[x] = z.name;
+            });
+            if (self.length === 0) self.push(`No commands yet.`);
 
-        let self = client.commands.map(cmd => cmd).filter(cmd => cmd.category === 'self');
-        self.forEach((z,x) => {
-            self[x] = z.name;
-        });
-        if (self.length === 0) self.push(`No commands yet.`);
+            const helpEmbed = new Discord.MessageEmbed()
+            .setColor('#72da7e')
+            .setTitle(`Available Commands`)
+            .setDescription(`Category \`DBL\` means things happening in the DBL server.\nCategory \`Luca\` means a simulation of Luca's features, available to everyone. (no mod commands)\nCategory \`Self\` means commands this bot has, that are unrelated to DBL and Luca.`)
+            .addField(`DBL`, dbl.join('\n'), true)
+            .addField(`Luca`, luca.join('\n'), true)
+            .addField(`Self`, self.join('\n'), true)
 
-        const helpEmbed = new Discord.MessageEmbed()
-        .setColor('#72da7e')
-        .setTitle(`Available Commands`)
-        .setDescription(`Category \`DBL\` means things happening in the DBL server.\nCategory \`Luca\` means a simulation of Luca's features, available to everyone. (no mod commands)\nCategory \`Self\` means commands this bot has, that are unrelated to DBL and Luca.`)
-        .addField(`DBL`, dbl.join('\n'), true)
-        .addField(`Luca`, luca.join('\n'), true)
-        .addField(`Self`, self.join('\n'), true)
-
-        return message.channel.send(helpEmbed).catch(err => console.error(err));
+            return message.channel.send(helpEmbed).catch(err => console.error(err));
+        } else if (command) {
+            
+        }
     }
 }
