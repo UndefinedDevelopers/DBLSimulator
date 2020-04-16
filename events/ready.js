7dcc9cbb069096c7bfa00ca4e03a08d87ffa09b1
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 09:31:54 -0400

    testing testing 1 2

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,12 @@
 module.exports = (client) => {
 	const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
-	bootchannel.messages.fetch().then(m => bootchannel.bulkDelete(m.size))
-	bootchannel.send("I am online")
+	async () => {
+	  let fetched;
+	  do {
+	    fetched = await bootchannel.fetchMessages({limit: 100});
+	    bootchannel.bulkDelete(fetched);
+	  }
+	  while(fetched.size >= 2);
+	}
+ 	bootchannel.send("Looks like I am back online!")
 }
