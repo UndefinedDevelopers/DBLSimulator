ff5c5e4f51ebbb55c3f257f0370af1388208bb04
CollierDevs <collier.bell20@gmail.com>
2020-04-16 09:47:11 -0500

    Mute Command

--- commands/mute.js
+++ commands/mute.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 const fse = require('fs-extra');
 
 module.exports = {
-    name: "unmute",
-    description: "Unmutes a user.",
+    name: "mute",
+    description: "mutes a user.",
     usage: '<mention or tag or id> [reason]',
     category: 'dbl',
-    example: 'dbl-unmute Aprixia',
+    example: 'dbl-mute Aprixia',
 
     async code(client, message, args) {
         let user = message.mentions.users.first();
@@ -20,31 +20,31 @@ module.exports = {
         fse.readJson(`reasons.json`, (err, reasons) => {
             let Case = Math.floor(Math.random() * (50000 - 20000) + 20000);
             let reason = args.slice(1).join(' ');
-            let unmuteEmbed = new Discord.MessageEmbed()
-                .setTitle(`Unmute | Case #${Case}`)
-                .setColor('fac10c')
+            let muteEmbed = new Discord.MessageEmbed()
+                .setTitle(`mute | Case #${Case}`)
+                .setColor('08de06')
             if (user.bot) {
-                unmuteEmbed.addField("Bot", `${user.tag}`, true);
+                muteEmbed.addField("Bot", `${user.tag}`, true);
             } else {
-                unmuteEmbed.addField("User", `${user.tag}`, true);
+                muteEmbed.addField("User", `${user.tag}`, true);
             }
             if (!reason) {
                 if (user.bot) {
-                    reason = reasons.botunmuteReason[Math.floor(Math.random() * (4 - 0) + 0)];
+                    reason = reasons.botmuteReason[Math.floor(Math.random() * (4 - 0) + 0)];
                 } else {
-                    reason = reasons.unmuteReason[Math.floor(Math.random() * (1 - 0) + 0)];
+                    reason = reasons.muteReason[Math.floor(Math.random() * (1 - 0) + 0)];
                 }
                 if (reason === 'Time\'s up') {
                     reason = reason+` #${Case-47}`;
-                    unmuteEmbed.addField(`Moderator`, `Topgg Discord Simulator#0086`, true);
+                    muteEmbed.addField(`Moderator`, `Topgg Discord Simulator#0086`, true);
                 } else {
-                    unmuteEmbed.addField(`Moderator`, `${message.author.tag}`, true);
+                    muteEmbed.addField(`Moderator`, `${message.author.tag}`, true);
                 }
             } else {
-                unmuteEmbed.addField(`Moderator`, `${message.author.tag}`, true);
+                muteEmbed.addField(`Moderator`, `${message.author.tag}`, true);
             }
-            unmuteEmbed.addField(`Reason`, reason);
-            message.channel.send(unmuteEmbed);
+            muteEmbed.addField(`Reason`, reason);
+            message.channel.send(muteEmbed);
         });
     }
 }
