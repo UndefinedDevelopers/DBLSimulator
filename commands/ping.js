a5084cf1ed01aba31020e78f5e5eb64dc3568ef4
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 12:20:40 -0400

    ping command

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+
+module.exports = {
+	name: "ping",
+	description: "Look at bot response times.",
+	usage: "",
+	category: "self",
+	example: "ping",
+
+	async code(client, message) {
+		const m = await message.channel.send("Pong...?")
+		m.edit("🏓 Pong!", {embed: {
+			color: 0x47aef0,
+			description: `💗 Websocket: \`${client.ws.ping}ms\`
+		📬 Message: \`${m.createdTimestamp - msg.createdTimestamp}ms\``
+		}})
+	}
+}
