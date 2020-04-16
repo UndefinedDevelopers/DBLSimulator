0a8b7e8e19f56c304354629b0f99636ab206528d
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 10:16:22 -0400

    add embed

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,9 @@ module.exports = async (client) => {
 	const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
 	let fetched = await bootchannel.messages.fetch({limit:100})
 	bootchannel.bulkDelete(fetched.size).then(() => {}).catch(err => console.error(err))
- 	bootchannel.send("@everyone I have rebooted!")
+ 	let embed = new Discord.MessageEmbed()
+ 	    .setTitle("I have rebooted!")
+ 	    .setColor("GREEN")
+ 	    .setTimestamp();
+ 	bootchannel.send("@everyone", {embed: embed})
 }
