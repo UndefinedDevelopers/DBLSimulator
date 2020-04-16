5cc55b13b328b13eb3646f48bd3cd3b76c3ac433
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-16 09:39:58 -0400

    pls work

--- events/ready.js
+++ events/ready.js
@@ -1,10 +1,10 @@
 module.exports = (client) => {
+	const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
 	async () => {
-		const bootchannel = client.channels.cache.find(c => c.id === "700328938609574049")
 		let fetched;
 		do {
-			fetched = await bootchannel.fetchMessages({limit: 100});
-			bootchannel.bulkDelete(fetched);
+			fetched = await bootchannel.messages.fetch({limit:100})
+			bootchannel.bulkDelete(fetched.size);
 		}
 		while(fetched.size >= 2);
 	}
