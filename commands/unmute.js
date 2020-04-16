7f95528a8c77727ecdae4ec4c94c0781d95b6d27
CollierDevs <collier.bell20@gmail.com>
2020-04-16 08:16:04 -0500

    Error Message to unmute
    Co-Authored-By: Aprixia <aprixia@users.noreply.github.com>

--- commands/unmute.js
+++ commands/unmute.js
@@ -11,11 +11,15 @@ module.exports = {
     async code(client, message, args) {
         let user = message.mentions.users.first();
         if (!user) {
+            const errEmbed = new Discord.MessageEmbed()
+            .setColor('#36393f')
+            .setDescription(`<:tickNo:700331270210846780> I may be blind, but I don't see that user here.`)
 
+            return message.channel.send(errEmbed).catch(err => err);
         }
         fse.readJson(`../reasons.json`, (err, reasons) => {
             let Case = Math.floor(Math.random() * (50000 - 20000) + 20000);
-            let reason = args.slice(0).join(' ');
+            let reason = args.slice(1).join(' ');
             if (!reason) {
                 reason = reasons.unmuteReason[Math.floor(Math.random() * reasons.unmuteReason.length-1)];
                 if (reason === 'Unmute Expired') {
