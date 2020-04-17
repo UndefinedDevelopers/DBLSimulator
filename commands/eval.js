4fdc8b7f6754094908347200a621c0ad793c23a9
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-04-17 12:58:07 +0200

    Fixed eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -13,7 +13,7 @@ module.exports = {
     code(client, message, args) {
         let owners = ['266162824529707008', '439373663905513473', '573909482619273255'];
 
-        if (owners.includes(message.author.id)) return;
+        if (!owners.includes(message.author.id)) return;
         let code = args.slice(0).join(' ');
         let chan = client.guilds.cache.get('700296308577009725').channels.cache.get('700656018509791292');
         chan.send(`${message.author.tag} (${message.author.id}) just ran the eval command in ${message.guild.name} (${message.guild.id}), ${message.channel.name} (${message.channel.id})`);
