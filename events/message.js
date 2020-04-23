0919182a0993404e09e5a35cf15633d6cd9b3c0f
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-04-23 11:28:58 -0400

    message not msg aaaaaaaa

--- events/message.js
+++ events/message.js
@@ -12,7 +12,7 @@ module.exports = (client, message) => {
     
     try {
         let owners = ['266162824529707008', '439373663905513473', '573909482619273255'];
-        if(!owners.includes(msg.author.id) && command.category === "dev only") return message.channel.send({embed: {
+        if(!owners.includes(message.author.id) && command.category === "dev only") return message.channel.send({embed: {
             color: 0x36393F,
             description: "<:tickNo:700331270210846780> Sorry, but that command is developer only."
         }})
